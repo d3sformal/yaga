cabe1f163d0fe521156daad66a3fefa697109e79
Drahomir Hanak <drahomir.hanak@gmail.com>
2022-12-15 17:47:59 +0100

    Add basic implementation of watched literals

---
+++ src/Bool_theory.cpp
@@ -0,0 +1,183 @@
+#include "Bool_theory.h"
+
+namespace perun {
+
+void Bool_theory::decide(Database&, Trail& trail, Variable var)
+{
+    if (var.type() == Variable::boolean)
+    {
+        auto& model = trail.model<bool>(Variable::boolean);
+        model.set_value(var.ord(), true);
+        trail.decide(var);
+    }
+}
+
+void Bool_theory::on_learned_clause(Database&, Trail&, Clause* learned)
+{
+    // initialize watched literals in the learned clause
+    watched_[(*learned)[0]].emplace_back(learned);
+    if (learned->size() > 1)
+    {
+        watched_[(*learned)[1]].emplace_back(learned);
+    }
+}
+
+std::optional<Clause> Bool_theory::initialize(Database& db, Trail& trail)
+{
+    const auto& model = trail.model<bool>(Variable::boolean);
+    
+    // allocate space for new variables if necessary
+    watched_.resize(model.num_vars());
+
+    if (trail.empty()) // initialize watch lists
+    {
+        // clear watch lists
+        for (auto& list : watched_)
+        {
+            list.clear();
+        }
+
+        // initialize watched literals
+        auto& asserted = db.asserted();
+        auto& learned = db.learned();
+        for (auto clause_list : {&asserted, &learned})
+        {
+            for (auto& clause : *clause_list)
+            {
+                if (clause.size() == 1) // propagate unit clauses
+                {
+                    watched_[clause[0]].emplace_back(Watched_clause{&clause});
+                    propagate_.push_back({clause[0], &clause});
+                }
+                else // non-unit clause
+                {
+                    watched_[clause[0]].emplace_back(&clause);
+                    watched_[clause[1]].emplace_back(&clause);
+                }
+            }
+        }
+    }
+
+    // propagate assigned variables
+    for (auto [var, reason] : trail.assigned(trail.decision_level()))
+    {
+        if (var.type() == Variable::boolean)
+        {
+            auto lit = model.value(var.ord()) ? Literal{var.ord()} : Literal{var.ord()}.negate();
+            propagate_.push_back({lit, reason});
+        }
+    }
+
+    return {};
+}
+
+std::optional<Clause> Bool_theory::propagate(Database& db, Trail& trail) 
+{
+    propagate_.clear();
+
+    auto& model = trail.model<bool>(Variable::boolean);
+    auto conflict = initialize(db, trail);
+
+    while (!conflict && !propagate_.empty())
+    {
+        auto [lit, reason] = propagate_.back();
+        propagate_.pop_back();
+
+        // propagate the literal if necessary
+        if (reason != nullptr && !model.is_defined(lit.var().ord()))
+        {
+            model.set_value(lit.var().ord(), !lit.is_negation());
+            trail.propagate(lit.var(), reason, trail.decision_level());
+        }
+        assert(is_true(model, lit));
+
+        conflict = falsified(model, lit.negate());
+    }
+    return conflict;
+}
+
+bool Bool_theory::replace_second_watch(const Model<bool>& model, Watched_clause& watch)
+{
+    auto& clause = *watch.clause;
+
+    assert(clause.size() >= 2);
+    assert(is_false(model, clause[1]));
+
+    if (is_true(model, clause[0]))
+    {
+        return false; 
+    }
+
+    if (clause.size() > 2)
+    {
+        const auto end = watch.index;
+        do
+        {
+            // check if the next literal is non-falsified
+            if (!is_false(model, clause[watch.index]))
+            {
+                std::swap(clause[1], clause[watch.index]);
+                watched_[clause[1]].push_back(watch);
+                return true;
+            }
+
+            // move to the next literal
+            if (++watch.index >= static_cast<int>(clause.size()))
+            {
+                watch.index = 2; // skip the watched literals
+            }
+        }
+        while (watch.index != end);
+    }
+    return false;
+}
+
+std::optional<Clause> Bool_theory::falsified(const Model<bool>& model, Literal falsified_lit)
+{
+    assert(is_false(model, falsified_lit));
+
+    auto& watchlist = watched_[falsified_lit];
+    for (std::size_t i = 0; i < watchlist.size();)
+    {
+        auto& watch = watchlist[i];
+        auto& clause = *watch.clause;
+
+        assert(clause.size() >= 1);
+        if (clause.size() == 1)
+        {
+            return clause; // the clause has just become empty
+        }
+
+        // move falsified literal to index 1
+        if (clause[0] == falsified_lit)
+        {
+            std::swap(clause[0], clause[1]);
+        }
+        assert(clause[1] == falsified_lit);
+
+        // skip satisfied clauses
+        if (is_true(model, clause[0]))
+        {
+            ++i;
+            continue;
+        }
+
+        if (replace_second_watch(model, watch))
+        {
+            std::swap(watch, watchlist.back());
+            watchlist.pop_back();
+        }
+        else // there is no other non-falsified literal in clause
+        {
+            if (is_false(model, clause[0])) // if the clause is false
+            {
+                return clause;
+            }
+            propagate_.push_back({clause[0], &clause});
+            ++i;
+        }
+    }
+    return {};
+}
+
+}
---
+++ src/Bool_theory.h
@@ -0,0 +1,88 @@
+#ifndef PERUN_BOOL_THEORY_H_
+#define PERUN_BOOL_THEORY_H_
+
+#include <optional>
+#include <vector>
+#include <algorithm>
+
+#include "Database.h"
+#include "Theory.h"
+#include "Trail.h"
+#include "Literal.h"
+#include "Literal_map.h"
+
+namespace perun {
+
+class Bool_theory : public Theory {
+public:
+    virtual ~Bool_theory() = default;
+
+    // run BCP to exhaustion
+    std::optional<Clause> propagate(Database& db, Trail& trail) override;
+
+    // decide value for variable var if it is a boolean variable
+    void decide(Database& db, Trail& trail, Variable var) override;
+
+    void on_learned_clause(Database& db, Trail& trail, Clause* learned) override;
+
+private:
+    // we move the watched literals to the first two position in each clause
+    struct Watched_clause {
+        // pointer to the watched clause in database
+        Clause* clause;
+        // the next index to check in clause
+        int index;
+
+        inline Watched_clause() {}
+        inline explicit Watched_clause(Clause* clause) : clause(clause), index(std::min<int>(2, clause->size())) {}
+    };
+
+    // map literal -> list of clauses in which it is watched
+    Literal_map<std::vector<Watched_clause>> watched_;
+    // stack of true literals to propagate with a pointer to the reason clause
+    std::vector<std::pair<Literal, Clause*>> propagate_;
+
+    /** Initialize propagate for all assigned literals at current level in @p trail
+     * 
+     * @param db clause database
+     * @param trail current trail
+     * @return conflict clause if a clause becomes false. Otherwise, nothing.
+     */
+    std::optional<Clause> initialize(Database& db, Trail& trail);
+
+    /** Move watch from recently falsified literal @p lit to some other literal. 
+     * 
+     * -# If some clause becomes unit, this method will propagate the implied literal by adding it to `propagate_`
+     * -# If some clause becomes false, this method will return a copy of that clause.
+     * 
+     * @param model current assignment of boolean variables 
+     * @param lit recently falsified literal in @p model
+     * @return conflict clause if a clause becomes flase. Otherwise, nothing.
+     */
+    std::optional<Clause> falsified(const Model<bool>& model, Literal lit);
+
+    /** Try to replace the second watched literal in @p watch with some other non-falsified literal
+     * 
+     * @param model current assignment of boolean variables
+     * @param watch 
+     * @return true iff the second watched literal has been replaced with some other non-falsified
+     * literal in the clause
+     */
+    bool replace_second_watch(const Model<bool>& model, Watched_clause& watch);
+
+    // check if lit is false in model
+    inline bool is_false(const Model<bool>& model, Literal lit) const 
+    { 
+        return model.is_defined(lit.var().ord()) && model.value(lit.var().ord()) == lit.is_negation(); 
+    }
+
+    // check if lit is true in model
+    inline bool is_true(const Model<bool>& model, Literal lit) const
+    {
+        return model.is_defined(lit.var().ord()) && model.value(lit.var().ord()) == !lit.is_negation();
+    }
+};
+
+}
+
+#endif // PERUN_BOOL_THEORY_H_
