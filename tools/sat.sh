a1f6f6aee4da9f9f8b681259771d89ba07523f40
Drahomir Hanak <drahomir.hanak@gmail.com>
2022-12-16 16:35:44 +0100

    Add statistics to the script output

--- tools/sat.sh
+++ tools/sat.sh
@@ -4,10 +4,11 @@ script_path=$(realpath "$0")
 script_dir=$(dirname "$script_path")
 bin=$(realpath "$script_dir/../build-release/sat")
 
-echo "path,result"
+echo "path,result,time,conflicts,decisions,restarts"
 for path in $@
 do
     out=$("$bin" "$path")
     res=$(echo "$out" | grep 'SAT')
-    echo "$path,$res"
+    stats=$(echo "$out" | grep = | cut -d'=' -f2 | sed 's/ //g' | paste -sd',') 
+    echo "$path,$res,$stats"
 done
