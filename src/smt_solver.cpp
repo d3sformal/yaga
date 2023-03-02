4bfe3d49137f6b06d04c27398366f3b5ca59689a
Martin Blicha <martin.blicha@gmail.com>
2023-03-02 16:08:45 +0100

    Add executable for SMT2

---
+++ src/smt_solver.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "Smt2_parser.h"
+
+using namespace perun;
+
+
+int main(int argc, char** argv)
+{
+    if (argc != 2)
+    {
+        std::cerr << "Usage: ./smt [input-path.smt2]" << std::endl;
+        return -1;
+    }
+
+    parser::Smt2_parser parser;
+    parser.parse_file(argv[1]);
+}
